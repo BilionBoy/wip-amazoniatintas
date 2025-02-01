20a873fd2260a61d7ac31110fe0eb71aeb790be9
BilionBoy <dhiogoandre26@gmail.com>
2025-02-01 15:53:12 -0400

    INFLECTIONS

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,10 +1,4 @@
 # config/initializers/inflections.rb
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.irregular "user", "users"
-  inflect.irregular "funcao", "funcoes"
-  inflect.irregular "estabelecimento", "estabelecimentos"
-  inflect.irregular "user_estabelecimento", "users_estabelecimentos"
-  inflect.irregular "categoria",  "categorias"
-  inflect.irregular "produto", "produtos"
 end
