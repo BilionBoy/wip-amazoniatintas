dc0d6b79768cf5bc2e6f3e3044ef2a89bce7fe85
kaikV <kaikev045@gmail.com>
2025-05-16 09:48:44 -0400

    feature : controller users do devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   resources :l_tipo_usuarios
   root 'home#index'
 
-  
   get 'home/index'
-  get "store/index"
-
-  # Rotas Scaffold
+  get 'store/index'
 
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
