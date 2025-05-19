812175e2f290eda76174af744148b15625ab67f4
kaikV <kaikev045@gmail.com>
2025-05-19 12:02:29 -0400

    feat: finalizando cadastro

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -30,32 +30,28 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
   # def cancel
   #   super
   # end
 
-  # protected
+  protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  # Permite parâmetros adicionais no momento do cadastro
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome, :l_tipo_usuario_id])
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  # Permite parâmetros adicionais no momento da edição da conta
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nome, :l_tipo_usuario_id])
+  end
 
-  # The path used after sign up.
+  # Caminho após o cadastro com sucesso
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
 
-  # The path used after sign up for inactive accounts.
+  # Caminho após cadastro de conta inativa
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-  sessions: 'users/sessions',
-  registrations: 'users/registrations'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
+
   resources :l_tipo_usuarios
+
   root 'home#index'
 
   get 'home/index'
   get 'store/index'
 
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
