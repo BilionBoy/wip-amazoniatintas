8c8fc343541263c4eeeeac224c43cca4c846c96d
Dhiogoandre <dhiogoandre26@gmail.com>
2025-05-26 04:20:33 -0400

    inflection: categoria

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -3,5 +3,6 @@
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular 'l_tipo_usuario', 'l_tipo_usuarios'
   inflect.irregular 'l_segmento',     'l_segmentos'
+  inflect.irregular 'l_categoria',    'l_categorias' 
 
 end
