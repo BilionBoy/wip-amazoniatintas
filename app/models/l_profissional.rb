f93630a8681f7d4896bb5e811ff2bf5524d834f5
Dhiogoandre <dhiogoandre26@gmail.com>
2025-05-30 01:29:09 -0400

    refactor: relacionamentos e validacoes model profissionais

--- app/models/l_profissional.rb
+++ app/models/l_profissional.rb
@@ -2,4 +2,9 @@
 
 class LProfissional < ApplicationRecord
   # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
+  has_one_attached :foto_profissional
+
+  validates :nome,          presence: true
+  validates :especialidade, presence: true
+  validates :descricao,     presence: true
 end
